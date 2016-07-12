b58b3461bd867ee1fbfd61b46110cf50f12c2cd0
jbroadway <johnny@johnnybroadway.com>
2016-07-12 11:04:25 -0500

    Moved window.ASLPlayer declaration into a single object definition

--- src/browser/aslplayer.js
+++ src/browser/aslplayer.js
@@ -1,26 +1,26 @@
-window.ASLPlayer = {};
+window.ASLPlayer = {
+	// Create a new ASL player
+	create: function (file, callback) {
+		callback ();
+	},
 
-// Create a new ASL player
-window.ASLPlayer.create = function (file, callback) {
-	callback ();
-};
+	// Play the ASL video, showing it if it is currently hidden.
+	play: function (callback) {
+		callback ();
+	},
 
-// Play the ASL video, showing it if it is currently hidden.
-window.ASLPlayer.play = function (callback) {
-	callback ();
-};
+	// Pause the ASL video
+	pause: function (callback) {
+		callback ();
+	},
 
-// Pause the ASL video
-window.ASLPlayer.pause = function (callback) {
-	callback ();
-};
+	// Change the seek time of the ASL video
+	seek: function (seek, callback) {
+		callback ();
+	},
 
-// Change the seek time of the ASL video
-window.ASLPlayer.seek = function (seek, callback) {
-	callback ();
-};
-
-// Hide the ASL player, pausing it if it is currently playing.
-window.ASLPlayer.hide = function (callback) {
-	callback ();
+	// Hide the ASL player, pausing it if it is currently playing.
+	hide: function (callback) {
+		callback ();
+	}
 };
--- www/aslplayer.js
+++ www/aslplayer.js
@@ -1,66 +1,66 @@
-window.ASLPlayer = {};
+window.ASLPlayer = {
+	// Create a new ASL player
+	create: function (file, callback) {
+		cordova.exec (
+			callback,
+			function (err) {
+				callback ();
+			},
+			"ASLPlayer",
+			"create",
+			[file]
+		);
+	},
 
-// Create a new ASL player
-window.ASLPlayer.create = function (file, callback) {
-	cordova.exec (
-		callback,
-		function (err) {
-			callback ();
-		},
-		"ASLPlayer",
-		"create",
-		[file]
-	);
-};
+	// Play the ASL video, showing it if it is currently hidden.
+	play: function (callback) {
+		cordova.exec (
+			callback,
+			function (err) {
+				callback ();
+			},
+			"ASLPlayer",
+			"play",
+			[]
+		);
+	},
 
-// Play the ASL video, showing it if it is currently hidden.
-window.ASLPlayer.play = function (callback) {
-	cordova.exec (
-		callback,
-		function (err) {
-			callback ();
-		},
-		"ASLPlayer",
-		"play",
-		[]
-	);
-};
+	// Pause the ASL video
+	pause: function (callback) {
+		cordova.exec (
+			callback,
+			function (err) {
+				callback ();
+			},
+			"ASLPlayer",
+			"pause",
+			[]
+		);
+	},
 
-// Pause the ASL video
-window.ASLPlayer.pause = function (callback) {
-	cordova.exec (
-		callback,
-		function (err) {
-			callback ();
-		},
-		"ASLPlayer",
-		"pause",
-		[]
-	);
-};
+	// Change the seek time of the ASL video
+	seek: function (seek, callback) {
+		cordova.exec (
+			callback,
+			function (err) {
+				callback ();
+			},
+			"ASLPlayer",
+			"seek",
+			[seek]
+		);
+	},
 
-// Change the seek time of the ASL video
-window.ASLPlayer.seek = function (seek, callback) {
-	cordova.exec (
-		callback,
-		function (err) {
-			callback ();
-		},
-		"ASLPlayer",
-		"seek",
-		[seek]
-	);
-};
-
-// Hide the ASL player, pausing it if it is currently playing.
-window.ASLPlayer.hide = function (callback) {
-	cordova.exec (
-		callback,
-		function (err) {
-			callback ();
-		},
-		"ASLPlayer",
-		"hide",
-		[]
-	);
+	// Hide the ASL player, pausing it if it is currently playing.
+	hide: function (callback) {
+		cordova.exec (
+			callback,
+			function (err) {
+				callback ();
+			},
+			"ASLPlayer",
+			"hide",
+			[]
+		);
+	}
 };
